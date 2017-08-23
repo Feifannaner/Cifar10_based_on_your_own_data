0678b636f0ba5ba4b48dbdfb5930a690e6fe215a
PengfeiSun <sunpengfeiyear@163.com>
2017-08-23 20:33:35 +0800

    Create collect photos one by one.py

---
+++ collect photos one by one.py
@@ -0,0 +1,30 @@
+import cv2
+import time 
+
+
+cv2.namedWindow('Camera',cv2.WINDOW_NORMAL)
+capture = cv2.VideoCapture(0)
+if(capture.isOpened()==False):
+	print('open camera error!')
+	
+else:
+	print('open sucess!')
+i = 0
+ret = capture.set(3,100)
+ret = capture.set(4,100)
+#success,frame = capture.read();
+while True:
+#	capture = cv2.VideoCapture(0)
+	ret,frame = capture.read()
+	frame = cv2.flip(frame,1)
+	cv2.imshow('Camera',frame)
+	key  = cv2.waitKey(10) & 0xff
+	if key == ord('c'):
+		cv2.imshow('camera',frame)
+		cv2.imwrite('pic{:>05}.jpg'.format(i),frame)
+		print('saved: {} files'.format(i))
+		i+=1
+	if key ==27:
+		break
+capture.release()
+cv2.destroyAllWindows()
